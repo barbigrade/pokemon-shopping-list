6ec5f423246e5fa305369bd1d188b0133d12dca1
Barbara Balen <barbarabalen@barbaras-air.localdomain>
2022-08-26 16:29:10 +0200

    add strikethrough function

--- src/components/Listitem.js
+++ src/components/Listitem.js
@@ -1,14 +1,22 @@
-export default function Listitem({ item, onRemoveListItem }) {
+import { useState } from "react";
+import "./Listitem.css";
 
+export default function Listitem({ item, onRemoveListItem }) {
   function handleClick() {
     onRemoveListItem(item);
   }
 
+  const [isDone, setIsDone] = useState(false);
+
+  function handleChange() {
+    setIsDone(!isDone);
+  }
+
   return (
-    <li>
-      <input type="checkbox" />
+    <li className={`${isDone ? "done" : "undone"}`}>
+      <input onChange={handleChange} type="checkbox" />
       {item.name}
       <button onClick={handleClick}>X</button>
     </li>
-  )
+  );
 }
